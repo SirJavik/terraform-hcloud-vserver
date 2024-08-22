36abeeb0e6b6054797c2aee15adb82e3c8662d54
Benjamin Schneider <ich@benjamin-schneider.com>
2024-08-22 19:46:08 +0200

    Reenable output

--- outputs.tf
+++ outputs.tf
@@ -10,11 +10,12 @@
 
 # Filename: outputs.tf
 # Description: 
-# Version: 1.1
+# Version: 1.2
 # Author: Benjamin Schneider <ich@benjamin-schneider.com>
 # Date: 2024-04-25
 # Last Modified: 2024-08-22
 # Changelog: 
+# 1.2 - Reenable volumes output
 # 1.1 - Disable volumes output
 # 1.0 - Initial version 
 
@@ -22,7 +23,7 @@ output "server" {
   value = local.server
 }
 
-#output "volumes" {
-#  value = {
-#  for volume in hcloud_volume.volume : volume.name => volume }
-#}
+output "volumes" {
+  value = {
+  for volume in hcloud_volume.volume : volume.name => volume }
+}
