c07d4a1c4e5697f83fca088e1f3f029ade5d8bab
Benjamin Schneider <ich@benjamin-schneider.com>
2024-08-23 21:31:14 +0200

    Remove suffix

--- volumes.tf
+++ volumes.tf
@@ -10,27 +10,21 @@
 
 # Filename: volumes.tf
 # Description: 
-# Version: 1.1
+# Version: 1.2
 # Author: Benjamin Schneider <ich@benjamin-schneider.com>
 # Date: 2024-04-26
 # Last Modified: 2024-08-23
 # Changelog: 
+# 1.2 - Remove random_integer for volume suffix
 # 1.1 - Add random_integer for volume suffix
 # 1.0 - Initial version 
-
-resource "random_integer" "vol_suffix" {
-  count = length(local.volumes_list) * length(local.server_list)
-  min   = 1000
-  max   = 9999
-}
 
 resource "hcloud_volume" "volume" {
   count = length(local.volumes_list) * length(local.server_list)
 
-  name = format("%s-%s-%d",
+  name = format("%s-%s",
     local.server_list[count.index % length(local.server_list)].name,
-    local.volumes_list[count.index % length(local.volumes_list)].name,
-    random_integer.vol_suffix[count.index].result
+    local.volumes_list[count.index % length(local.volumes_list)].name
   )
 
   size      = local.volumes_list[count.index % length(local.volumes_list)].size
