0d0654bad31886846dd8dc98494a5194867a39b0
Benjamin Schneider <ich@benjamin-schneider.com>
2024-08-23 21:07:34 +0200

    Add random int as suffix

--- volumes.tf
+++ volumes.tf
@@ -10,19 +10,27 @@
 
 # Filename: volumes.tf
 # Description: 
-# Version: 1.0
+# Version: 1.1
 # Author: Benjamin Schneider <ich@benjamin-schneider.com>
 # Date: 2024-04-26
-# Last Modified: 2024-04-26
+# Last Modified: 2024-08-23
 # Changelog: 
+# 1.1 - Add random_integer for volume suffix
 # 1.0 - Initial version 
 
-resource "hcloud_volume" "volume" {
-  count = length(var.volumes) * length(local.server)
+resource "random_integer" "vol_suffix" {
+  count = length(var.volumes) * length(local.server_list)
+  min   = 1000
+  max   = 9999
+}
 
-  name = format("%s-%s",
+resource "hcloud_volume" "volume" {
+  count = length(var.volumes) * length(local.server_list)
+
+  name = format("%s-%s-%d",
     local.server_list[count.index % length(local.server_list)].name,
-    local.volumes_list[count.index % length(local.volumes_list)].name
+    local.volumes_list[count.index % length(local.volumes_list)].name,
+    random_integer.vol_suffix[count.index].result
   )
 
   size      = local.volumes_list[count.index % length(local.volumes_list)].size
